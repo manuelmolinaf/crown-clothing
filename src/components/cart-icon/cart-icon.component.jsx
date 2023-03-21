78d74113f53f6280b7c1de85b4d1a5c798b2299d
manuelmolinaf <manuelmolinaf@outlook.com>
2023-03-21 14:59:17 -0400

    added cart total

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,9 +8,14 @@ import { CartContext } from "../../context/cart.context";
 const CartIcon = () => {
 
   const { cartCount } = useContext(CartContext);
+  
+  const handleClose = () => {
+    // Do something to close the dropdown
+    // For example, you can use state to toggle the visibility of the dropdown
+  };
 
   return (
-    <Dropdown className="d-flex align-items-center" align='end' autoClose={false}>
+    <Dropdown className="d-flex align-items-center" align='end' autoClose={false} onClose={handleClose}>
       <Dropdown.Toggle variant="transparent" className="p-0" id="dropdown-basic">
         <span className="position-relative">
           <FontAwesomeIcon className="text-white fs-2" icon={faBagShopping} />
@@ -26,7 +31,7 @@ const CartIcon = () => {
       </Dropdown.Toggle>
 
       <Dropdown.Menu className="p-0">
-        <CartDropdownMenu />
+        <CartDropdownMenu onClose={handleClose} />
       </Dropdown.Menu>
     </Dropdown>
 
