2d493e5c251f11a35f5f544b116e5bd0728c8c5d
manuelmolinaf <manuelmolinaf@outlook.com>
2023-03-22 02:32:42 -0400

    added data to firebase and updated shop component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,38 @@
-import { ProductsContext } from '../../context/products.context';
+import { categoriesContext } from '../../context/categories.context';
 import { useContext } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProductCard from '../../components/product-card/product-card.component';
-
+import { Container } from 'react-bootstrap';
+import { Fragment } from 'react';
 const Shop = () => {
 
-  const {products} = useContext(ProductsContext);
+  const {categoriesMap} = useContext(categoriesContext);
+  console.log(categoriesMap);
 
   return (
     
-    <Row className="no-gutters d-flex">
-      {products.map((product) => {
-        return(
-          <Col key={product.id} xs={3} className='p-3' style={{minWidth:'300px'}}>
-            <ProductCard key={product.id} product={product} />
-          </Col>
-  
-        )
-      })}
-    </Row>
+    <Container>
+      <Row className="no-gutters d-flex">
+        {/* {categoriesMap['hats'].map((product) => {
+          return(
+            <Col key={product.id} xs={3} className='p-3' style={{minWidth:'300px'}}>
+              <ProductCard key={product.id} product={product} />
+            </Col>
+          )
+        })} */}
+
+        {
+          Object.keys(categoriesMap).map( title =>categoriesMap[title].slice(0,4).map((product) => {
+            return(
+              <Col key={product.id} xs={3} className='p-3' style={{minWidth:'300px'}}>
+                <ProductCard key={product.id} product={product} />
+              </Col>
+            )}))
+        }
+
+      </Row>
+    </Container>
   
   )
 }
