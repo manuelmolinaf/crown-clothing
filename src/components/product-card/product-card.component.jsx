b9fdf3422e0d45f967d3ae7a25289d031689ba33
manuelmolinaf <manuelmolinaf@outlook.com>
2023-03-21 20:19:28 -0400

    shanged shop stye

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,26 +1,55 @@
 
 import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
-import Button from '../button/button.component';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import './product-card.styles.scss';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  const {addItemToCart} = useContext(CartContext);
+  const { addItemToCart } = useContext(CartContext);
 
-  const addToCartHandler = () =>{
+  const addToCartHandler = () => {
     addItemToCart(product);
   }
 
   return (
-    <div className='product-card-container'>
-      <img className='rounded' src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <Button buttonType='inverted' onClick={addToCartHandler}>Add to cart</Button>
-    </div>
+    // <div className='product-card-container'>
+    //   <img className='rounded' src={imageUrl} alt={`${name}`} />
+    //   <div className='footer'>
+    //     <span className='name'>{name}</span>
+    //     <span className='price'>{price}</span>
+    //   </div>
+    //   <Button buttonType='inverted' onClick={addToCartHandler}>Add to cart</Button>
+    // </div>
+
+    <Card>
+      <Card.Img variant="top" src={imageUrl} style={{height:'360px', width:'100%', objectFit:'cover'}} />
+      <Card.Body>
+        <div className='d-flex align-items-center'>
+          <span className='fs-4 fw-bold'>
+            {name}
+          </span>
+          <span className='fs-4 fw-bold ms-3'>
+          {'$'+price}
+          </span>
+
+          <div className='ms-auto'>
+            <Button variant="dark" onClick={addToCartHandler}>
+              <FontAwesomeIcon icon={faCartShopping} className='me-2'></FontAwesomeIcon>
+              Add to cart
+            </Button>
+          </div>
+          
+        </div>
+        <div className='d-flex justify-content-center'>
+          
+        </div>
+      </Card.Body>
+    </Card>
+
   );
 };
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,27 @@
 import { ProductsContext } from '../../context/products.context';
 import { useContext } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ProductCard from '../../components/product-card/product-card.component';
-import './shop.styles.scss';
+
 const Shop = () => {
 
   const {products} = useContext(ProductsContext);
 
   return (
-    <div className='products-container'>
+
+    
+    
+    <Row className="no-gutters -3">
       {products.map((product) => {
         return(
-          <ProductCard key={product.id} product={product} />
+          <Col xs={3} className='p-3'>
+            <ProductCard key={product.id} product={product} />
+          </Col>
           
         )
       })}
-    </div>
+    </Row>
   
   )
 }
